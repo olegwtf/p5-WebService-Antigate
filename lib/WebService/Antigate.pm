769d14df05626e06ecb3bc5ddf18d70f3f3ac6de
Oleg G <verdrehung@gmail.com>
2017-10-28 02:45:37 +0700

    update Changes and bump version

--- lib/WebService/Antigate.pm
+++ lib/WebService/Antigate.pm
@@ -4,7 +4,7 @@ use strict;
 use LWP::UserAgent ();
 use Carp ();
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 our $DOMAIN = 'anti-captcha.com'; # service domain often changes because of the abuse
 our $WAIT   = 220;                # default time that recognize() or upload() can work
